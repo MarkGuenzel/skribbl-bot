743cf989a2db49bd16986bc7606773c57eb8894e
Mark Günzel <mark.guenzel@gmx.de>
2026-07-17 14:42:42 +0200

    Electron setup

---
+++ src/ui/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <p>Hello World</p>
+      <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import path from "path"
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  base: "./",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/ui"),
+    },
+  },
+  build: {
+    outDir: "dist-react"
+  },
+  server: {
+    port: 5123,
+    strictPort: true
+  }
+})
