105b096640ebb05906b4ebfd72298d0f04f8cc72
Mark Günzel <mark.guenzel@gmx.de>
2026-08-08 18:13:12 +0200

    Added more setup and loading of the game and sidebar

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,14 +1,54 @@
-import { app, BrowserWindow } from "electron"
+import { app, BaseWindow, WebContentsView } from "electron"
 import path from "path"
-import { isDev } from "./util.js";
+import { getPreloadPath } from "./utils.js";
+
+const SIDEBAR_WIDTH = 320;
+
+const loadWebContents = (mainWindow: BaseWindow) => {
+    // Skribbl
+    const gameView = new WebContentsView();
+    mainWindow.contentView.addChildView(gameView);
+    gameView.webContents.loadURL("https://skribbl.io");
+    
+    // Sidebar
+    const sidebar = new WebContentsView({
+        webPreferences: {
+            preload: getPreloadPath()
+        }
+    })
+    mainWindow.contentView.addChildView(sidebar);
+    sidebar.webContents.loadURL("http://localhost:5123");
+
+    const layout = () => {
+        const { width, height } = mainWindow.getContentBounds();
+        gameView.setBounds({ 
+            x: 0,
+            y: 0,
+            width: width - SIDEBAR_WIDTH,
+            height: height
+        });
+        sidebar.setBounds({
+            x: width - SIDEBAR_WIDTH,
+            y: 0,
+            width: SIDEBAR_WIDTH,
+            height: height
+        })
+    }
+
+    layout();
+    mainWindow.on("resize", layout);
+    mainWindow.on("enter-full-screen", layout);
+    mainWindow.on("leave-full-screen", layout);
+    mainWindow.on("maximize", layout);
+    mainWindow.on("unmaximize", layout);
+}
 
 app.on("ready", () => {
-    const mainWindow = new BrowserWindow({});
-    
-    if (isDev()) {
-        mainWindow.loadURL("http://localhost:5123");
-    }
-    else {
-        mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
-    }
+    const mainWindow = new BaseWindow({
+        width: 1200,
+        height: 800,
+    });
+
+    loadWebContents(mainWindow);
+
 })
